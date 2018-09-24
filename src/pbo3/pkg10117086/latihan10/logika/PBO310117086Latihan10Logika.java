0a00ee58cc00b424064b4a62c8442752cd214b0b
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:28:13 -0700

    Logika

---
+++ src/pbo3/pkg10117086/latihan10/logika/PBO310117086Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan10.logika;
+
+/**
+ *
+ * @author Lenovo
+ *  Nama     : RD Muhammad Djulfikar BU
+ *  NIM      : 10117086
+ *  Kelas    : IF3
+ *  Minggu 23 september 2018
+ */
+public class PBO310117086Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
